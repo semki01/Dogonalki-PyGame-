e22adc8679722be64c5d37d633f6d6ee555eeeeb
Semki 01 <91430213+semki01@users.noreply.github.com>
2022-01-16 19:18:55 +0200

    Add files via upload

---
+++ dogonalki.py
@@ -0,0 +1,51 @@
+from pygame import *
+
+window = display.set_mode((700, 500))
+display.set_caption('Догонялки')
+background = transform.scale(image.load('background.png'), (700, 500))
+
+#данные о спрайте картинки
+x1 = 300
+y1 = 300
+
+x2 = 100
+y2 = 300
+
+sprite_1 = transform.scale(image.load('sprite1.png'), (100, 100))
+sprite_2 = transform.scale(image.load('sprite2.png'), (100, 100))
+speed = 10
+
+#игровой цикл
+
+run = True
+clock = time.Clock()
+FPS = 60
+
+while run:
+    window.blit(background,(0,0))
+    window.blit(sprite_1,(x1,y1))
+    window.blit(sprite_2,(x2,y2))
+
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    keys_pressed = key.get_pressed()
+    if keys_pressed[K_LEFT] and x1 > 5:
+        x1 -= speed
+    if keys_pressed[K_RIGHT] and x1 < 595:
+        x1 += speed
+    if keys_pressed[K_UP] and y1 > 5:
+        y1 -= speed
+    if keys_pressed[K_DOWN] and y1 < 395:
+        y1 += speed
+    
+    if keys_pressed[K_a] and x2 > 5:
+        x2 -= speed
+    if keys_pressed[K_d] and x2 < 595:
+        x2 += speed
+    if keys_pressed[K_w] and y2 > 5:
+        y2 -= speed
+    if keys_pressed[K_s] and y2 < 395:
+        y2 += speed
+    display.update()
+    clock.tick(FPS)
